8386a57ddf2843042ff2cef98a3d3087d231a1e1
Htocki <pasztownia@gmail.com>
2020-11-28 19:31:13 +0300

    Реализована поочередная смена цвета точек.

--- application/src/Main.cpp
+++ application/src/Main.cpp
@@ -7,8 +7,6 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
-using Position = sf::Vector2f;
-
 struct Size {
     sf::Uint32 width { 0 };
     sf::Uint32 height { 0 };
@@ -16,7 +14,7 @@ struct Size {
 
 class Grid {
   public:
-    Grid(const Position& position, const Size& size, uint32_t cell_size, sf::Color color) {
+    Grid(const sf::Vector2f& position, const Size& size, uint32_t cell_size, sf::Color color) {
         for (sf::Uint32 i { 0 }; i <= size.width; ++i) {
             lines_.push_back({ 
                 sf::Vertex(sf::Vector2f(position.x + i * cell_size, position.y), color), 
@@ -44,7 +42,7 @@ class Grid {
 
 class Points {
   public:
-    Points(const Position& position, const Size& count, sf::Uint32 distance, sf::Uint32 radius, sf::Color color) {
+    Points(const sf::Vector2f& position, const Size& count, sf::Uint32 distance, sf::Uint32 radius, sf::Color color) {
         points_.resize(count.width * count.height);
         
         std::size_t k { 0 };
@@ -62,18 +60,23 @@ class Points {
         for (auto& point : points_) { point.setFillColor(color); }
     }
 
-    void handleEvent(const sf::RenderWindow& window, const sf::Event& event, const sf::Color color) {
-        if (event.type == sf::Event::MouseButtonPressed) {
-            if (event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2i pixel { sf::Mouse::getPosition(window) };
-                sf::Vector2f coords { window.mapPixelToCoords(pixel) };
-                for (auto& point : points_) {
-                    if (point.getGlobalBounds().contains(coords)) {
-                        point.setFillColor(color);
-                    }
+    void setPointFillColor(const sf::Vector2f& position, const sf::Color& color) {
+        for (auto& point : points_) {
+            if (point.getGlobalBounds().contains(position)) {
+                if (point.getFillColor() == sf::Color::White) {
+                    point.setFillColor(color);
                 }
             }
         }
+    }
+
+    bool isContainsNotFilledPoint(const sf::Vector2f& position) {
+        for (auto& point : points_) {
+            if (point.getGlobalBounds().contains(position) && point.getFillColor() == sf::Color::White) {
+                return true;
+            }
+        }
+        return false;
     }
 
     void draw(sf::RenderWindow* window) {
@@ -90,10 +93,15 @@ class Points {
 
 class Player {
   public:
-    Player(const sf::String& name, const sf::Color& color)
+    Player(const sf::String& name, const sf::Color& color, bool active)
         : name_ { name }
         , color_ { color }
+        , active_ { active }
     {}
+
+    void activate() { active_ = true; }
+    void deactivate() { active_ = false; }
+    bool isActive() const { return active_; }
 
     const sf::Color& getColor() const { return color_; }
     const sf::String& getName() const { return name_; }
@@ -101,6 +109,7 @@ class Player {
   private:
     sf::String name_;
     sf::Color color_;
+    bool active_;
 };
 
 class Assets {
@@ -128,7 +137,7 @@ class Assets {
 class Label {
   public:
     Label(
-        const Position& position, const sf::String& string, const sf::Font& font,
+        const sf::Vector2f& position, const sf::String& string, const sf::Font& font,
         const sf::Color& color, unsigned int character_size, sf::Uint32 style) 
     {
         text_.setPosition(position);
@@ -150,34 +159,47 @@ class Label {
 int main() {
     try {
         sf::RenderWindow window(sf::VideoMode(800, 600), "POINTS");
+        window.setFramerateLimit(70);
 
-        Grid grid { Position { 150, 50 }, Size { 20, 20 }, 25, sf::Color::Black };
-        Points points { Position { 150, 50 }, Size { 21, 21 }, 25, 4, sf::Color::White };
+        Grid grid { sf::Vector2f { 150, 50 }, Size { 20, 20 }, 25, sf::Color::Black };
+        Points points { sf::Vector2f { 150, 50 }, Size { 21, 21 }, 25, 4, sf::Color::White };
     
-        Player player1 { "PlayerOne", sf::Color::Red };
-        Player player2 { "PlayerDwo", sf::Color::Blue };
+        Player player1 { "first", sf::Color::Red, true };
+        Player player2 { "second", sf::Color::Blue, false };
 
-        Label label1 { Position { 9, 40 }, player1.getName(), Assets::instance().getSansationFont(), player1.getColor(), 26, sf::Text::Bold };
-        Label label2 { Position { 659, 40 }, player2.getName(), Assets::instance().getSansationFont(), player2.getColor(), 26, sf::Text::Bold };
-    
         while (window.isOpen()) {
             sf::Event event;    
             while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-                    window.close();
-                }
-                points.handleEvent(window, event, sf::Color::Red);    
+                if (event.type == sf::Event::Closed) { window.close(); }
+                
+                if (event.type == sf::Event::MouseButtonPressed) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        sf::Vector2f coords { window.mapPixelToCoords(sf::Mouse::getPosition(window)) };
+
+                        if (player1.isActive() && !player2.isActive()) {
+                            if (points.isContainsNotFilledPoint(coords)) {
+                                points.setPointFillColor(coords, player1.getColor());
+                                player1.deactivate();
+                                player2.activate();
+                            }
+                        } else {
+                            if (points.isContainsNotFilledPoint(coords)) {
+                                points.setPointFillColor(coords, player2.getColor());
+                                player1.activate();
+                                player2.deactivate();
+                            }
+                        }
+                    }
+                }    
             }
             window.clear(sf::Color::White);
             grid.draw(&window);
             points.draw(&window);
-            label1.draw(&window);
-            label2.draw(&window);
             window.display();
         }
-
-    } catch (const std::exception& exception) {
-        std::cerr << exception.what() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
+    
     return 0;
 }
